7ab50aa73723366af2f12caf9cf010cf3df44d56
Amit Singh <omit9090@gmail.com>
2023-11-24 21:45:07 +0600

    deployment okk

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
-const app: Application = express()
+const app: Application = express() 
 
 app.use(express.json())
 app.use(cors())
+
+
+app.get('/',(req: Request,res:Response)=>{
+    res.send('App is Running')
+})
 
 export default app
