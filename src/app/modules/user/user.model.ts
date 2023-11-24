d12d441162b540c4ccb14f369166f3b3fdd479f5
Amit Singh <omit9090@gmail.com>
2023-11-25 01:09:13 +0600

    user model

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,1 +1,39 @@
-// import { Schema, model, connect } from 'mongoose';
+import { Schema, model, connect } from 'mongoose';
+import { TAddress, TOrder, TUser, TUserName } from './user.interface';
+
+
+
+const userNameSchema = new Schema<TUserName>({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+});
+
+const addressSchema = new Schema<TAddress>({
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+});
+
+
+const orderSchema = new Schema<TOrder>({
+    productName: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+});
+
+
+
+const userSchema = new Schema<TUser>({
+    userId: { type: Number, unique: true, required: true },
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    fullName: { type: userNameSchema, required: true },
+    age: { type: Number, required: true },
+    email: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+    hobbies: { type: [String], default: [] },
+    address: { type: addressSchema, required: true },
+    orders: { type: [orderSchema], default: [] },
+});
+
+const User = model<TUser>('User', userSchema);
